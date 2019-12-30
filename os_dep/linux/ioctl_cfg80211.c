09116acb7c58be762cba57b8aa8ed7913b73dbc2
sardnailap <sardnailap@gmail.com>
2019-12-30 18:45:17 +0100

    this statement may fall through

--- os_dep/linux/ioctl_cfg80211.c
+++ os_dep/linux/ioctl_cfg80211.c
@@ -2363,6 +2363,7 @@ static int cfg80211_rtw_change_iface(struct wiphy *wiphy,
 	case NL80211_IFTYPE_P2P_CLIENT:
 		is_p2p = _TRUE;
 	#endif
+		/* Falls through */
 	case NL80211_IFTYPE_STATION:
 		networkType = Ndis802_11Infrastructure;
 
@@ -2387,6 +2388,7 @@ static int cfg80211_rtw_change_iface(struct wiphy *wiphy,
 	case NL80211_IFTYPE_P2P_GO:
 		is_p2p = _TRUE;
 	#endif
+		/* Falls through */
 	case NL80211_IFTYPE_AP:
 		networkType = Ndis802_11APMode;
 
